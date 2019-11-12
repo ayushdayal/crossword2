07e2ec9f0a7b3198b3c6587d259f3040d8832c15
ayushdayal <ayushdayal1998@gmail.com>
2019-11-12 15:01:54 +0530

    ocr working ayush

--- ocr.py
+++ ocr.py
@@ -1,6 +1,4 @@
 import pytesseract as tess
-
-tess.pytesseract.tesseract_cmd = r'C:\Users\Johnson Minz\AppData\Local\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
 im = Image.open("right.jpg")
@@ -10,6 +8,8 @@ text = tess.image_to_string(im)
 i = 0
 last = i
 for x in text:
+    if x == 'o':
+        x = ' '
     if x == ')':
         last = i
     i = i + 1
@@ -17,16 +17,16 @@ for x in text:
 newtext = text[:last].split(')')
 newtext2 = []
 temp = []
-
+dic = ()
 for x in newtext:
     temp.clear()
     # print(x)
-    x.replace('\n\n', ' ')
+    x = x.replace('\n', '')
     asd = x.split(' ', 1)
     # print(asd)
     temp.append(asd[0])
     temp.append(asd[1].split('('))
     print(temp)
-    newtext2.append(temp)
+    newtext2.append(temp[:])
 
 print(newtext2)
